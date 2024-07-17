72e0ac5ec6b67c813ab9d7371c224a5e76cb9860
itamar2315 <itamar2315@gmail.com>
2024-07-17 23:39:01 +0300

    levels 00 - 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,6 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    return reversed.split("").reverse().join(""); 
+    //turns the string to array reverses it and makes it a string again
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,42 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) { // didn't know about forEach() and includes() 
+
+    const newArray = [];
+    isInBothArrays:
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < remove.length; j++){
+            if(remove[j] === array[i]){
+                continue isInBothArrays;
+            }
+        }
+        newArray.push(array[i]);
+    }
+    return newArray;
 };
+/* TOP solution:
+const removeFromArray = function (array, ...args) {
+    // create a new empty array
+    const newArray = [];
+    // use forEach to go through the array
+    array.forEach((item) => {
+      // push every element into the new array
+      // UNLESS it is included in the function arguments
+      // so we create a new array with every item, except those that should be removed
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    // and return that array
+    return newArray;
+  };
+  
+  // A simpler, but more advanced way to do it is to use the 'filter' function,
+  // which basically does what we did with the forEach above.
+  
+  // const removeFromArray = function(array, ...args) {
+  //   return array.filter(val => !args.includes(val))
+  // }
+  */
+  
 
 // Do not edit below this line
 module.exports = removeFromArray;
