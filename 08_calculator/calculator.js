4fa573b374ba72040359bfa2298622b590a734fc
itamar2315 <itamar2315@gmail.com>
2024-07-18 12:25:33 +0300

    solve levels 05-08 and start of 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,46 @@
-const add = function() {
+const add = function(...args) {
+  let total = 0;
+  args.forEach(num => {
+    total += num;
+  });
+  return total
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  let total = args[0];
+  for(let i = 1; i < args.length; i++){
+    total -= args[i];
+  }
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  array.forEach(num => {
+    total += num;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  array.forEach(num => {
+    total *= num;
+  });
+  return total
 };
 
-const power = function() {
-	
+const power = function(num, powerOf) {
+  return Math.pow(num, powerOf)
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let total = 1;
+  for(let i = 1; i <= num; i++){
+    total *= i
+  }
+  return total;
 	
 };
 
